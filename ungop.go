5f0dc5ead4b67322dcd48290eab35e19c69b62bd
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-11-24 16:13:59 +0900

    -l and -h option

--- ungop.go
+++ ungop.go
@@ -1,57 +1,79 @@
 package main
 
 import "archive/zip"
+import "flag"
 import "fmt"
 import "io"
 import "os"
 
+var listFlag = flag.Bool("l", false, "listing")
+var directory = flag.String("d", "", "expand directory")
+
 func main() {
-	if len(os.Args) < 2 {
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
 		return
 	}
-	reader, readerErr := os.Open(os.Args[1])
+	zipFileName := args[0]
+	reader, readerErr := os.Open(zipFileName)
 	if readerErr != nil {
-		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[1], readerErr.Error())
+		fmt.Fprintf(os.Stderr, "%s: %s\n", zipFileName, readerErr.Error())
 		return
 	}
 	defer reader.Close()
 	finfo, finfoErr := reader.Stat()
 	if finfoErr != nil {
-		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[1], finfoErr.Error())
+		fmt.Fprintf(os.Stderr, "%s: %s\n", zipFileName, finfoErr.Error())
 		return
 	}
 	zipReader, zipReaderErr := zip.NewReader(reader, finfo.Size())
 	if zipReaderErr != nil {
-		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[1], zipReaderErr.Error())
+		fmt.Fprintf(os.Stderr, "%s: %s\n", zipFileName, zipReaderErr.Error())
 		return
 	}
+	if len(*directory) > 0 {
+		os.Chdir(*directory)
+	}
+	files := map[string]bool{}
+	for _, fname := range args[1:] {
+		files[fname] = true
+	}
 	for _, f := range zipReader.File {
+		if len(files) > 0 && !files[f.Name] {
+			continue
+		}
+		if *listFlag {
+			fmt.Println(f.Name)
+			continue
+		}
 		zipFileReader, zipFileReaderErr := f.Open()
 		if zipFileReaderErr != nil {
 			fmt.Fprintf(os.Stderr, "%s: %s: %s\n",
-				os.Args[1],
+				zipFileName,
 				f.Name,
 				zipFileReaderErr.Error())
-			continue
-		}
-		unzipWriter, unzipWriterErr := os.Create(f.Name)
-		if unzipWriterErr != nil {
-			fmt.Fprintf(os.Stderr, "%s: %s: %s\n",
-				os.Args[1],
-				f.Name,
-				unzipWriterErr.Error())
 		} else {
-			_, err := io.Copy(unzipWriter, zipFileReader)
-			if err != nil {
+			unzipWriter, unzipWriterErr := os.Create(f.Name)
+			if unzipWriterErr != nil {
 				fmt.Fprintf(os.Stderr, "%s: %s: %s\n",
-					os.Args[1],
+					zipFileName,
 					f.Name,
-					err.Error())
+					unzipWriterErr.Error())
 			} else {
-				fmt.Println(f.Name)
+				_, err := io.Copy(unzipWriter, zipFileReader)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s: %s: %s\n",
+						zipFileName,
+						f.Name,
+						err.Error())
+				} else {
+					fmt.Println(f.Name)
+				}
+				unzipWriter.Close()
 			}
-			zipFileReader.Close()
 		}
-		unzipWriter.Close()
+		zipFileReader.Close()
 	}
 }
