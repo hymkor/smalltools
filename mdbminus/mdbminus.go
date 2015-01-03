e5b86a7ba9ef2ccd00f1a60f0c369be149eff0f6
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-01-03 20:17:03 +0900

    go fmt: mdbminus

--- mdbminus/mdbminus.go
+++ mdbminus/mdbminus.go
@@ -9,38 +9,38 @@ import (
 	_ "github.com/mattn/go-adodb"
 )
 
-func main(){
+func main() {
 	if len(os.Args) < 3 {
-		fmt.Fprintf(os.Stderr,"Usage: %s MDBFILENAME SQL\n",os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s MDBFILENAME SQL\n", os.Args[0])
 		return
 	}
-	if _,err := os.Stat(os.Args[1]) ; err != nil {
-		fmt.Fprintf(os.Stderr,"%s: Not Found(%s)",os.Args[1],err)
+	if _, err := os.Stat(os.Args[1]); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: Not Found(%s)", os.Args[1], err)
 		return
 	}
-	db,dbErr := sql.Open("adodb",
+	db, dbErr := sql.Open("adodb",
 		"Provider=Microsoft.Jet.OLEDB.4.0;Data Source="+os.Args[1])
 	if dbErr != nil {
-		fmt.Fprintln(os.Stderr,dbErr)
+		fmt.Fprintln(os.Stderr, dbErr)
 		return
 	}
 	defer db.Close()
 	fmt.Println(os.Args[2])
-	if strings.HasPrefix(strings.ToLower(os.Args[2]),"select") {
-		rows,err := db.Query(os.Args[2])
+	if strings.HasPrefix(strings.ToLower(os.Args[2]), "select") {
+		rows, err := db.Query(os.Args[2])
 		if err != nil {
-			fmt.Fprintln(os.Stderr,err)
+			fmt.Fprintln(os.Stderr, err)
 			return
 		}
 		defer rows.Close()
-		cols,err := rows.Columns()
+		cols, err := rows.Columns()
 		if err != nil {
-			fmt.Fprintln(os.Stderr,err)
+			fmt.Fprintln(os.Stderr, err)
 			return
 		}
-		values := make([]interface{},len(cols),len(cols))
-		pValues := make([]interface{},len(cols),len(cols))
-		for i := 0 ; i < len(cols) ; i++ {
+		values := make([]interface{}, len(cols), len(cols))
+		pValues := make([]interface{}, len(cols), len(cols))
+		for i := 0; i < len(cols); i++ {
 			if i > 0 {
 				fmt.Print(",")
 			}
@@ -49,22 +49,22 @@ func main(){
 		}
 		fmt.Println()
 		for rows.Next() {
-			if err := rows.Scan(pValues...) ; err != nil {
-				fmt.Fprintln(os.Stderr,err)
+			if err := rows.Scan(pValues...); err != nil {
+				fmt.Fprintln(os.Stderr, err)
 				return
 			}
-			for i,cell := range values {
+			for i, cell := range values {
 				if i > 0 {
 					fmt.Print(",")
 				}
-				fmt.Printf("%v",cell)
+				fmt.Printf("%v", cell)
 			}
 			fmt.Println()
 		}
-	}else{
-		_,resultErr := db.Exec(os.Args[2])
+	} else {
+		_, resultErr := db.Exec(os.Args[2])
 		if resultErr != nil {
-			fmt.Fprintf(os.Stderr,"%s: %s\n",os.Args[2],resultErr.Error())
+			fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[2], resultErr.Error())
 			return
 		}
 		fmt.Println("done")
