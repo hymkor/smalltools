4d70d97497301ad70ad56b0f88dc5bd3c65667e8
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-01-03 20:17:24 +0900

    go fmt: stub

--- stub/stub.go
+++ stub/stub.go
@@ -6,16 +6,16 @@ import "strings"
 
 import "github.com/zetamatta/nyagos/Src/dos"
 
-func main(){
-	exeName,_ := dos.GetModuleFileName()
-	logName := strings.Replace(exeName,".exe",".out",-1)
-	logFp,logFpErr := os.Create(logName)
+func main() {
+	exeName, _ := dos.GetModuleFileName()
+	logName := strings.Replace(exeName, ".exe", ".out", -1)
+	logFp, logFpErr := os.Create(logName)
 	if logFpErr != nil {
-		fmt.Fprintf(os.Stderr,"%s: %s\n",os.Args[0],logFpErr.Error())
+		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], logFpErr.Error())
 		return
 	}
-	for i,arg1 := range os.Args {
-		fmt.Fprintf(logFp,"[%d]='%s'\n",i,arg1)
+	for i, arg1 := range os.Args {
+		fmt.Fprintf(logFp, "[%d]='%s'\n", i, arg1)
 	}
 	logFp.Close()
 }
